13011b8d962ce18cffa0f2129ef6d24aeba1fa5f
Adam Scheblein <adamscheblein@gmail.com>
2025-10-15 09:51:17 -0400

    updated goSDKVersion

--- output/genesyscloud/languageunderstanding_miners/genesyscloud_languageunderstanding_miners_proxy.go
+++ output/genesyscloud/languageunderstanding_miners/genesyscloud_languageunderstanding_miners_proxy.go
@@ -3,7 +3,7 @@ package languageunderstanding_miners
 import (
 	"context"
 	"fmt"
-	"github.com/mypurecloud/platform-client-sdk-go/v133/platformclientv2"
+	"github.com/mypurecloud/platform-client-sdk-go/v165/platformclientv2"
 	"log"
 )
 
